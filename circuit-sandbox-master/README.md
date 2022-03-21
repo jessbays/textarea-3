ad6b4cd18c71cc9f70951bfe3437a255356371e5
jessbays <jessbaysic@gmail.com>
2022-03-21 11:42:59 +0800

    first deploy

---
+++ circuit-sandbox-master/README.md
@@ -0,0 +1,11 @@
+# Circuit Sandbox
+
+A simulator for experimenting with circuits, written entirely in Javascript.
+
+Click... draw... simulate. There's no installation. 
+
+A teaching tool for [spinningnumbers.org](https://spinningnumbers.org).
+
+[Circuit Sandbox documentation](https://spinningnumbers.org/a/circuit-sandbox.html)
+
+[Circuit Sandbox simulator](https://spinningnumbers.org/circuit-sandbox/index.html)
---
+++ server.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var express = require('express');
+var ShareDB = require('sharedb');
+var WebSocket = require('ws');
+var WebSocketJSONStream = require('@teamwork/websocket-json-stream');
+var path = require('path');
+
+var backend = new ShareDB();
+createDoc(startServer);
+
+// Create initial document then fire callback
+function createDoc(callback) {
+  var connection = backend.connect();
+  var doc = connection.get('examples', 'textarea');
+  doc.fetch(function(err) {
+    if (err) throw err;
+    if (doc.type === null) {
+      doc.create({content: ''}, callback);
+      return;
+    }
+    callback();
+  });
+}
+
+function startServer() {
+  // Create a web server to serve files and listen to WebSocket connections
+  var app = express();
+  // app.use(express.static('circuit-sandbox-master'));
+  // app.use(express.static('static'));
+
+  // app.use(express.static(path.join(__dirname, 'static')));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'static/index.html'));
+  // });
+
+  app.use(express.static(path.join(__dirname, 'circuit-sandbox-master')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'circuit-sandbox-master/index.html'));
+  });
+
+  var server = http.createServer(app);
+
+  // Connect any incoming WebSocket connection to ShareDB
+  var wss = new WebSocket.Server({server: server});
+  wss.on('connection', function(ws) {
+    var stream = new WebSocketJSONStream(ws);
+    backend.listen(stream);
+  });
+
+  server.listen(8080);
+  console.log('Listening on http://localhost:8080');
+}
